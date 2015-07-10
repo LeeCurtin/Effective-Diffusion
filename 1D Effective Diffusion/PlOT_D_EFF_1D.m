52f9fbbf12fe8896c7099601a900fcd4832608e2
LeeCurtin <pmxlc1@lpmlc.nottingham.ac.uk>
2015-07-10 12:07:51 +0100

    Still not accepting arbritary subdomain number

--- 1D Effective Diffusion/PlOT_D_EFF_1D.m
+++ 1D Effective Diffusion/PlOT_D_EFF_1D.m
@@ -8,7 +8,7 @@ for i = 1:length(t)
     plot(x,u(:,i)) 
     hold on 
 %     plot(y,v(:,i))
-    axis([0 x(end) 0 20]);
+    axis([0 x(end) 0 1]);
     ylabel('Concentration');
     xlabel('Water,Wafer,Water,Brain');
     hold off
